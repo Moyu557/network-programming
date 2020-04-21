9159df34a191e49bc8ba0d152e406a208e1744a5
墨钰 <47318099+Moyu557@users.noreply.github.com>
2020-04-21 16:55:25 +0800

    Add files via upload

---
+++ tcp_client.c
@@ -0,0 +1,63 @@
+/*************************************************************************
+	> File Name: tcp_client.c
+	> Author: moyu
+	> Mail: 
+	> Created Time: 2020年04月21日 星期二 09时58分26秒
+ ************************************************************************/
+
+#include "tcp_header.h"
+
+int main()
+{
+    int sfd;
+    
+    //创建套接字
+    if((sfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket");
+        return -1;
+    }
+
+
+    //创建结构体
+    struct sockaddr_in myaddr;
+    
+    //清空
+    memset(&myaddr, 0, sizeof(myaddr));
+
+    //补充结构体
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_port = htons(CLIENT_PORT);
+    myaddr.sin_addr.s_addr = inet_addr(SERVER_IPADDR);
+    
+    //请求连接
+    if (connect(sfd, (struct sockaddr*)&myaddr, sizeof(myaddr)) < 0)
+    {
+        perror("connect");
+        return -1;
+    }
+
+
+    //读写数据
+    while (1)
+    {
+        char buf[BUFSIZE];
+        
+        //从键盘读取一行字符
+        fgets(buf, BUFSIZE, stdin);
+
+
+        //写入通信套接字
+        write(sfd, buf, BUFSIZE);
+
+        //判断退出
+        if (strcmp(buf, "quit\n") == 0)
+        {
+            printf("Bye.\n");
+            break;
+        }
+    }
+
+    //关闭套接字
+    close(sfd);
+}
